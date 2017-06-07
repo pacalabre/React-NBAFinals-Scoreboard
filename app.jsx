c08eabe02f2fff4ef5232ad7cc4cffa20b676527
Paul Calabrese <pacalabre@gmail.com>
2017-06-06 21:26:20 -0700

    took the hard coded info and added it in with functions and props

--- app.jsx
+++ app.jsx
@@ -1,24 +1,53 @@
+function Gametime(props) {
+	return (
+		<section>
+			<h3 className="game-number">{props.game}</h3>
+			<h4 className="game-date">{props.gameday}</h4>
+		</section>
+	)
+}
+
+function CavsScore(props) {
+	return (
+		<section className="section-cle">
+			<img className="team-logo" src="./images/cavs.png" />
+			<section className="section-cle-score">
+				<h2 className="section-h2">{props.team}</h2>
+				<h4 className="score">{props.score}</h4>
+			</section>
+		</section>
+	)
+}
+
+function GSWScore(props) {
+	return (
+		<section className="section-gsw">
+			<img className="team-logo" src="./images/gsw.png" />
+			<section  className="section-gsw-score">
+				<h2 className="section-h2">{ props.team }</h2>
+				<h4 className="score">{ props.score }</h4>
+			</section>
+		</section>
+	)
+}
+
+function SeriesScore(props) {
+	return (
+		<section>
+			<h2 className="series-score">{props.timeleft}</h2>
+			<h5 className="series-score">{props.seriesScore}</h5>
+		</section>
+	)
+}
+
+
 function Application() {
 	return (
 		<main className="app-section">
-			<h3 className="game-number">Game 1</h3>
-			<h4 className="game-date">Thursday, June 1</h4>
-			<section className="section-cle">
-				<img className="team-logo" src="./images/cavs.png" />
-				<section className="section-cle-score">
-					<h2 className="section-h2">CLE</h2>
-					<h4 className="score">91</h4>
-				</section>
-			</section>
-			<section className="section-gsw">
-				<img className="team-logo" src="./images/gsw.png" />
-				<section  className="section-gsw-score">
-					<h2 className="section-h2">GSW</h2>
-					<h4 className="score">113</h4>
-				</section>
-			</section>
-			<h2 className="series-score">FINAL</h2>
-			<h5 className="series-score">CLE 0 Games GSW 1</h5>
+			<Gametime game="Game 1" gameday="Thursday, June 1" />
+			<CavsScore team="Cavs" score="91" />
+			<GSWScore team="GSW" score="113" />
+			<SeriesScore timeleft="FINAL" seriesScore="CLE 0 Games GSW 1" />
 		</main>
 		)
 }
